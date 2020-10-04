16e00d495b319e586bf63a35cdc1fab2d07b5408
errorow <quso12358@naver.com>
2020-10-04 16:54:09 +0900

    change message

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 4000;
+
 const server = http.createServer((req, res) => {
  res.statusCode = 200;
  res.setHeader('Content-Type', 'text/plain');
- res.end('Hello World\n');
+ const today = new Date();
+ res.end(today.toString());
 });
 server.listen(port, hostname, () => {
  console.log(`Server running at http://${hostname}:${port}/`);
